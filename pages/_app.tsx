06d7c04d197603d8a8c40653a2950c351ae18c84
dannleed <59970198+dannleed@users.noreply.github.com>
2022-10-19 12:56:12 +0300

    fix types

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
+import { Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
 import { AppProps } from 'next/app';
 
-const App = ({ Component, pageProps }: AppProps) => {
+type AppWithAuthProps = AppProps & {
+	pageProps: { session: Session };
+};
+
+const App = ({ Component, pageProps }: AppWithAuthProps) => {
 	return (
 		<SessionProvider session={pageProps.session}>
 			<Component {...pageProps} />
