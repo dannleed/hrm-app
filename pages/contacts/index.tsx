221c103e61f8ad070ec151c753eeacb8868d25c5
dannleed <59970198+dannleed@users.noreply.github.com>
2022-12-16 02:16:06 +0200

    add vcard export and fix images

--- pages/contacts/index.tsx
+++ pages/contacts/index.tsx
@@ -1,9 +1,10 @@
-import { Center, Group, Avatar, Text, Flex, Select, Paper, Box } from '@mantine/core';
-import { IconPhoneCall, IconAt, IconMapPin } from '@tabler/icons';
+import { Center, Group, Avatar, Text, Flex, Select, Paper, ActionIcon } from '@mantine/core';
+import { IconPhoneCall, IconAt, IconMapPin, IconDownload } from '@tabler/icons';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { createStyles } from '@mantine/core';
 
 type Contact = {
 	'category': string;
@@ -16,6 +17,45 @@ type Contact = {
 	'posX': number | '';
 	'posY': number | '';
 	'photo': string | null;
+};
+
+const useStyles = createStyles((_theme, _params, getRef) => ({
+	navbar: {
+		zIndex: 100,
+		backdropFilter: 'blur(10px)',
+		height: 'auto',
+	},
+	image: {
+		objectFit: 'cover',
+	},
+	link: {
+		textDecoration: 'none',
+	},
+	actionIcon: {
+		backdropFilter: 'blur(1.3px)',
+	},
+}));
+
+const exportToVCard = (contact: Contact) => {
+	const vCard = `BEGIN:VCARD
+VERSION:3.0
+${contact.name ? `N:${contact.name}` : ''}
+${contact.name ? `FN:${contact.name}` : ''}
+ORG:Євразія
+${contact.position ? `TITLE:${contact.position}` : ''}
+${contact.phone ? `TEL;TYPE=WORK,VOICE:${contact.phone}` : ''}
+${contact.address ? `ADR;TYPE=WORK:;;Київ, ${contact.address}` : ''}
+${contact.google_maps_url ? `URL;TYPE=Google Maps:${contact.google_maps_url}` : ''}
+${contact.email ? `EMAIL;TYPE=PREF,INTERNET:${contact.email}` : ''}
+END:VCARD`;
+
+	const blob = new Blob([vCard], { type: 'text/vcard' });
+	const url = URL.createObjectURL(blob);
+
+	const link = document.createElement('a');
+	link.href = url;
+	link.download = `${contact.name}.vcf`;
+	link.click();
 };
 
 export async function getServerSideProps() {
@@ -33,10 +73,20 @@ export async function getServerSideProps() {
 }
 
 function Contacts({ data }: { data: Contact[] }) {
+	const { classes } = useStyles();
+
 	const labels = ['Всі', ...new Set(data.map((contact) => contact.category))].map((label) => ({
 		label,
 		value: label,
 	}));
+
+	const [isMobile, setIsMobile] = useState(false);
+
+	useEffect(() => {
+		if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+			setIsMobile(true);
+		}
+	}, []);
 
 	const [filteredData, setFilteredData] = useState(data);
 
@@ -60,12 +110,7 @@ function Contacts({ data }: { data: Contact[] }) {
 				w={'100%'}
 				top={0}
 				p={10}
-				style={{
-					zIndex: 100,
-					backdropFilter: 'blur(10px)',
-					WebkitBackdropFilter: 'blur(10px)',
-					height: 'auto',
-				}}
+				className={classes.navbar}
 			>
 				<Flex align={'center'} justify={'center'} wrap={'nowrap'} gap={10} m={'auto'}>
 					<Text weight={'bold'}>Контакт-лист </Text>
@@ -97,6 +142,8 @@ function Contacts({ data }: { data: Contact[] }) {
 							}
 						});
 
+						const isAbleToSave = contact.name && contact.phone && isMobile;
+
 						const posX = contact.posX !== '' ? `${contact.posX}%` : '50%';
 						const posY = contact.posY !== '' ? `${contact.posY}%` : '50%';
 
@@ -109,20 +156,32 @@ function Contacts({ data }: { data: Contact[] }) {
 								key={contact.name}
 								w={'100%'}
 								miw={300}
+								pos={'relative'}
 							>
+								{isAbleToSave && (
+									<ActionIcon
+										onClick={() => exportToVCard(contact)}
+										pos={'absolute'}
+										top={4}
+										right={4}
+										bg={'#ffffff70'}
+										className={classes.actionIcon}
+									>
+										<IconDownload size={16} />
+									</ActionIcon>
+								)}
 								<Group noWrap spacing={10}>
 									{imageLink ? (
 										<Avatar size={96} radius="md">
 											<Image
 												src={imageLink}
 												alt={contact.name}
-												fill={true}
+												sizes="192px"
+												quality={90}
 												loading="lazy"
-												style={{
-													objectFit: 'cover',
-													objectPosition: `${posX} ${posY}`,
-													maxWidth: '100%',
-												}}
+												className={classes.image}
+												style={{ objectPosition: `${posX} ${posY}` }}
+												fill
 											/>
 										</Avatar>
 									) : (
@@ -142,9 +201,7 @@ function Contacts({ data }: { data: Contact[] }) {
 												<IconAt stroke={1.5} size={16} />
 												<Link
 													href={`mailto:${contact.email.toLowerCase()}`}
-													style={{
-														textDecoration: 'none',
-													}}
+													className={classes.link}
 												>
 													<Text size="xs" color="dimmed">
 														{contact.email.toLowerCase()}
@@ -155,12 +212,7 @@ function Contacts({ data }: { data: Contact[] }) {
 										{contact.address && (
 											<Group noWrap spacing={5} mt={3}>
 												<IconMapPin stroke={1.5} size={16} />
-												<Link
-													href={`${contact.google_maps_url}`}
-													style={{
-														textDecoration: 'none',
-													}}
-												>
+												<Link href={`${contact.google_maps_url}`} className={classes.link}>
 													<Text size="xs" color="dimmed">
 														{contact.address}
 													</Text>
@@ -175,12 +227,7 @@ function Contacts({ data }: { data: Contact[] }) {
 														phone && (
 															<Group noWrap spacing={5} key={phone}>
 																<IconPhoneCall stroke={1.5} size={16} />
-																<Link
-																	href={`tel:${phone}`}
-																	style={{
-																		textDecoration: 'none',
-																	}}
-																>
+																<Link href={`tel:${phone}`} className={classes.link}>
 																	<Text size="xs" color="dimmed">
 																		{phone}
 																	</Text>
