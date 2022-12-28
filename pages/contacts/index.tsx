082355788d49a5ebe93f1e1e77d9404b75968c4f
Даниил Стрелец <danstrelets@Elizavetas-MacBook-Air.local>
2022-12-28 18:41:31 +0200

    add feauters

--- pages/contacts/index.tsx
+++ pages/contacts/index.tsx
@@ -1,10 +1,24 @@
-import { Center, Group, Avatar, Text, Flex, Select, Paper, ActionIcon } from '@mantine/core';
+import {
+	Center,
+	Group,
+	Avatar,
+	Text,
+	Flex,
+	Paper,
+	ActionIcon,
+	Tabs,
+	ScrollArea,
+	Title,
+	createStyles,
+	MANTINE_COLORS,
+} from '@mantine/core';
 import { IconPhoneCall, IconAt, IconMapPin, IconDownload } from '@tabler/icons';
 import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
-import { useEffect, useState } from 'react';
-import { createStyles } from '@mantine/core';
+import { useEffect, useLayoutEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { useDebouncedState } from '@mantine/hooks';
 
 type Contact = {
 	'category': string;
@@ -19,7 +33,11 @@ type Contact = {
 	'photo': string | null;
 };
 
-const useStyles = createStyles((_theme, _params, getRef) => ({
+interface IContactData {
+	[key: string]: Contact[];
+}
+
+const useStyles = createStyles((_theme, _params, _getRef) => ({
 	navbar: {
 		zIndex: 100,
 		backdropFilter: 'blur(10px)',
@@ -58,6 +76,49 @@ END:VCARD`;
 	link.click();
 };
 
+const clamp = (value: number) => Math.max(0, value);
+
+const isBetween = (value: number, floor: number, ceil: number) => value >= floor && value <= ceil;
+
+// hooks
+const useScrollspy = (ids: string[], offset: number = 0) => {
+	const [activeId, setActiveId] = useDebouncedState('', 50);
+
+	useLayoutEffect(() => {
+		const listener = () => {
+			const scroll = window.pageYOffset;
+
+			const position = ids
+				.map((id) => {
+					const element = document.getElementById(id);
+
+					if (!element) return { id, top: -1, bottom: -1 };
+
+					const rect = element.getBoundingClientRect();
+					const top = clamp(rect.top + scroll - offset);
+					const bottom = clamp(rect.bottom + scroll - offset);
+
+					return { id, top, bottom };
+				})
+				.find(({ top, bottom }) => isBetween(scroll, top, bottom));
+
+			setActiveId(position?.id || '');
+		};
+
+		listener();
+
+		window.addEventListener('resize', listener);
+		window.addEventListener('scroll', listener);
+
+		return () => {
+			window.removeEventListener('resize', listener);
+			window.removeEventListener('scroll', listener);
+		};
+	}, [ids, offset]);
+
+	return activeId;
+};
+
 export async function getServerSideProps() {
 	const res = await fetch(
 		'https://script.google.com/macros/s/AKfycbxFPyaXUCJVJv6Sco2_vfho504HmpCZs3E2H99hGohwNUv55F3rpElSWF9KfLUnsS-0/exec',
@@ -73,177 +134,256 @@ export async function getServerSideProps() {
 }
 
 function Contacts({ data }: { data: Contact[] }) {
+	const router = useRouter();
 	const { classes } = useStyles();
 
-	const labels = ['Всі', ...new Set(data.map((contact) => contact.category))].map((label) => ({
+	const groupedContacts: IContactData = data.reduce((group, contact) => {
+		const { category } = contact;
+		group[category] = group[category] ?? [];
+		group[category].push(contact);
+		return group;
+	}, {});
+
+	const labels = [...new Set(data.map((contact) => contact.category))].map((label) => ({
 		label,
-		value: label,
+		value: label.replace(' ', '_'),
 	}));
 
 	const [isMobile, setIsMobile] = useState(false);
 
 	useEffect(() => {
-		if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+		if (/iPhone|iPod|Android/i.test(navigator.userAgent)) {
 			setIsMobile(true);
 		}
 	}, []);
 
-	const [filteredData, setFilteredData] = useState(data);
+	const activeId = useScrollspy(
+		labels.map((el) => el.value),
+		70,
+	);
 
-	const handleSelect = (value: string) => {
-		if (value === 'Всі') {
-			setFilteredData(data);
-		} else {
-			setFilteredData(data.filter((contact) => contact.category === value));
+	useEffect(() => {
+		const element = document.getElementById(activeId + '_menu');
+		if (element) {
+			// 👇 Will scroll smoothly to the top of the next section
+			element.scrollIntoView({ behavior: 'smooth' });
 		}
-	};
+	}, [activeId]);
 
 	return (
-		<>
+		<div>
 			<Head>
 				<title>Контакт лист</title>
 			</Head>
 			<Paper
 				shadow={'xs'}
 				bg={'transparent'}
-				pos={'sticky'}
+				pos={'fixed'}
 				w={'100%'}
 				top={0}
 				p={10}
+				pb={5}
 				className={classes.navbar}
 			>
-				<Flex align={'center'} justify={'center'} wrap={'nowrap'} gap={10} m={'auto'}>
-					<Text weight={'bold'}>Контакт-лист </Text>
-					<Select
-						data={labels}
-						defaultValue={'Всі'}
-						onChange={handleSelect}
-						variant="unstyled"
-						radius="xl"
-						size="md"
-						iconWidth={20}
-					/>
-				</Flex>
+				<Tabs
+					defaultValue={activeId}
+					value={activeId}
+					unstyled
+					styles={(theme) => ({
+						tab: {
+							...theme.fn.focusStyles(),
+							backgroundColor: '#ffffff75',
+							color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.colors.gray[9],
+							border: `1px solid #f3f3f380`,
+							padding: `${theme.spacing.xs}px ${theme.spacing.md}px`,
+							cursor: 'pointer',
+							fontSize: theme.fontSizes.sm,
+							display: 'flex',
+							alignItems: 'center',
+							width: 'max-content',
+							whiteSpace: 'nowrap',
+							borderRadius: theme.radius.md,
+							gap: '10px',
+
+							'&[data-active]': {
+								backgroundColor: theme.colors.yellow[7],
+								borderColor: theme.colors.yellow[7],
+								color: theme.white,
+							},
+						},
+
+						tabsList: {
+							display: 'flex',
+							gap: '4px',
+							justifyContent: 'center',
+						},
+					})}
+				>
+					<ScrollArea offsetScrollbars scrollbarSize={5} type="never">
+						<Tabs.List>
+							{labels.map((el) => (
+								<a
+									href={`/contacts#${el.value}`}
+									key={el.value}
+									style={{
+										textDecoration: 'none',
+									}}
+									id={el.value + '_menu'}
+								>
+									<Tabs.Tab key={el.value} value={el.value}>
+										{el.label}
+									</Tabs.Tab>
+								</a>
+							))}
+						</Tabs.List>
+					</ScrollArea>
+				</Tabs>
 			</Paper>
 			<Center>
 				<Flex align={'center'} justify={'start'} wrap={'wrap'} maw={600} gap={10} p={10} miw={350}>
-					{filteredData.map((contact) => {
-						let imageLink;
-						if (contact.photo) {
-							const rx = /com\/(.+)/gm;
-							imageLink = `https://i.gyazo.com/${rx.exec(contact.photo)![1]}.jpg`;
-						}
-
-						const phones = contact.phone?.split(',').map((phone) => {
-							if (phone.trim().startsWith('0800')) {
-								return phone.replace(/(\d{1})(\d{3})(\d{2})(\d{2})/, '$1 $2 $3 $4 ');
-							} else {
-								return phone.trim().replace(/(\d{3})(\d{3})(\d{2})(\d{2})/, '$1 $2 $3 $4 ');
-							}
-						});
-
-						const isAbleToSave = contact.name && contact.phone && isMobile;
-
-						const posX = contact.posX !== '' ? `${contact.posX}%` : '50%';
-						const posY = contact.posY !== '' ? `${contact.posY}%` : '50%';
-
+					{Object.keys(groupedContacts).map((label) => {
 						return (
-							<Paper
-								shadow={'lg'}
-								p={'xs'}
-								withBorder={true}
-								radius={'lg'}
-								key={contact.name}
-								w={'100%'}
-								miw={300}
-								pos={'relative'}
-							>
-								{isAbleToSave && (
-									<ActionIcon
-										onClick={() => exportToVCard(contact)}
-										pos={'absolute'}
-										top={4}
-										right={4}
-										bg={'#ffffff70'}
-										className={classes.actionIcon}
-									>
-										<IconDownload size={16} />
-									</ActionIcon>
-								)}
-								<Group noWrap spacing={10}>
-									{imageLink ? (
-										<Avatar size={96} radius="md">
-											<Image
-												src={imageLink}
-												alt={contact.name}
-												sizes="192px"
-												quality={90}
-												loading="lazy"
-												className={classes.image}
-												style={{ objectPosition: `${posX} ${posY}` }}
-												fill
-											/>
-										</Avatar>
-									) : (
-										<Avatar size={96} radius="md"></Avatar>
-									)}
-									<div>
-										<Text size="xs" sx={{ textTransform: 'uppercase' }} weight={700} color="dimmed">
-											{contact.position}
-										</Text>
+							<>
+								<section
+									style={{ all: 'inherit', padding: 0, paddingTop: 70, marginBottom: -70 }}
+									id={label.replace(' ', '_')}
+								>
+									<Title order={2} key={label}>
+										{label}
+									</Title>
+									{groupedContacts[label].map((contact) => {
+										let imageLink;
+										if (contact.photo) {
+											const rx = /com\/(.+)/gm;
+											imageLink = `https://i.gyazo.com/${rx.exec(contact.photo)![1]}.jpg`;
+										}
 
-										<Text size="lg" weight={500}>
-											{contact.name}
-										</Text>
+										const phones = contact.phone?.split(',').map((phone) => {
+											if (phone.trim().startsWith('0800')) {
+												return phone.replace(/(\d{1})(\d{3})(\d{2})(\d{2})/, '$1 $2 $3 $4 ');
+											} else {
+												return phone.trim().replace(/(\d{3})(\d{3})(\d{2})(\d{2})/, '$1 $2 $3 $4 ');
+											}
+										});
 
-										{contact.email && (
-											<Group noWrap spacing={5} mt={3}>
-												<IconAt stroke={1.5} size={16} />
-												<Link
-													href={`mailto:${contact.email.toLowerCase()}`}
-													className={classes.link}
-												>
-													<Text size="xs" color="dimmed">
-														{contact.email.toLowerCase()}
-													</Text>
-												</Link>
-											</Group>
-										)}
-										{contact.address && (
-											<Group noWrap spacing={5} mt={3}>
-												<IconMapPin stroke={1.5} size={16} />
-												<Link href={`${contact.google_maps_url}`} className={classes.link}>
-													<Text size="xs" color="dimmed">
-														{contact.address}
-													</Text>
-												</Link>
-											</Group>
-										)}
+										const isAbleToSave = contact.name && contact.phone && isMobile;
 
-										<Flex align={'center'} justify={'start'} wrap={'wrap'} gap={10} mt={3}>
-											{!!phones!.length &&
-												phones?.map(
-													(phone) =>
-														phone && (
-															<Group noWrap spacing={5} key={phone}>
-																<IconPhoneCall stroke={1.5} size={16} />
-																<Link href={`tel:${phone}`} className={classes.link}>
+										const posX = contact.posX !== '' ? `${contact.posX}%` : '50%';
+										const posY = contact.posY !== '' ? `${contact.posY}%` : '50%';
+
+										const colorByName =
+											MANTINE_COLORS[
+												contact.name.split(' ')[0].charCodeAt(0) % MANTINE_COLORS.length ?? 0
+											];
+
+										return (
+											<Paper
+												shadow={'lg'}
+												p={'xs'}
+												withBorder={true}
+												radius={'lg'}
+												key={contact.name}
+												w={'100%'}
+												miw={300}
+												pos={'relative'}
+											>
+												{isAbleToSave && (
+													<ActionIcon
+														onClick={() => exportToVCard(contact)}
+														pos={'absolute'}
+														top={4}
+														right={4}
+														bg={'#ffffff70'}
+														className={classes.actionIcon}
+													>
+														<IconDownload size={16} />
+													</ActionIcon>
+												)}
+												<Group noWrap spacing={10}>
+													{imageLink ? (
+														<Avatar size={96} radius="md">
+															<Image
+																src={imageLink}
+																alt={contact.name}
+																sizes="192px"
+																quality={90}
+																loading="lazy"
+																className={classes.image}
+																style={{ objectPosition: `${posX} ${posY}` }}
+																fill
+															/>
+														</Avatar>
+													) : (
+														<Avatar size={96} radius="md" color={colorByName}>
+															{contact.name.split(' ')[0][0] + contact.name.split(' ')[1][0]}
+														</Avatar>
+													)}
+													<div>
+														<Text
+															size="xs"
+															sx={{ textTransform: 'uppercase' }}
+															weight={700}
+															color="dimmed"
+														>
+															{contact.position}
+														</Text>
+
+														<Text size="lg" weight={500}>
+															{contact.name}
+														</Text>
+
+														{contact.email && (
+															<Group noWrap spacing={5} mt={3}>
+																<IconAt stroke={1.5} size={16} />
+																<Link
+																	href={`mailto:${contact.email.toLowerCase()}`}
+																	className={classes.link}
+																>
 																	<Text size="xs" color="dimmed">
-																		{phone}
+																		{contact.email.toLowerCase()}
 																	</Text>
 																</Link>
 															</Group>
-														),
-												)}
-										</Flex>
-									</div>
-								</Group>
-							</Paper>
+														)}
+														{contact.address && (
+															<Group noWrap spacing={5} mt={3}>
+																<IconMapPin stroke={1.5} size={16} />
+																<Link href={`${contact.google_maps_url}`} className={classes.link}>
+																	<Text size="xs" color="dimmed">
+																		{contact.address}
+																	</Text>
+																</Link>
+															</Group>
+														)}
+
+														<Flex align={'center'} justify={'start'} wrap={'wrap'} gap={10} mt={3}>
+															{!!phones!.length &&
+																phones?.map(
+																	(phone) =>
+																		phone && (
+																			<Group noWrap spacing={5} key={phone}>
+																				<IconPhoneCall stroke={1.5} size={16} />
+																				<Link href={`tel:${phone}`} className={classes.link}>
+																					<Text size="xs" color="dimmed">
+																						{phone}
+																					</Text>
+																				</Link>
+																			</Group>
+																		),
+																)}
+														</Flex>
+													</div>
+												</Group>
+											</Paper>
+										);
+									})}
+								</section>
+							</>
 						);
 					})}
 				</Flex>
 			</Center>
-		</>
+		</div>
 	);
 }
 
